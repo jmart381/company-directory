ebd5a979cc451ce4073150f4e23ed38168edf229
Juan C Martinez <stc.classcontent@gmail.com>
2024-02-05 18:07:33 -0600

    fixed rounded images and cards

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -12,7 +12,7 @@ const username = faker.internet.userName({firstName: firstname, lastName:lastnam
 <template>
     <!-- {{ fullname }} <br /> -->
     <div class="border border-gray-500 rounded-lg shadow bg-gray-300 h-full">
-        <img :src="faker.image.urlLoremFlickr({category: 'capybara'})"/>
+        <img :src="faker.image.urlLoremFlickr({category: 'capybara'})" class="rounded-t-lg"/>
         <div class="p-4">
             <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
             <p class="mb-3 font-normal text-gray-800">{{ job_title }}</p>
